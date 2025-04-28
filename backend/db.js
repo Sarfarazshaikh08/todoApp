26d65f67829f1b8cc48ef5ab1978e0fd9c920d51
Sarfarazshaikh08 <mohdsarfaraz371@gmail.com>
2025-04-28 14:15:49 +0000

    removed connection string

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://admin:sarfaraz%409852@cluster0.7c6mr.mongodb.net/todos")
+mongoose.connect("")
 const todoSchema = mongoose.Schema({
     title: String,
     description: String,
